a35e4108aa19e6aed51fae227090c6c58859cd51
PeaceCwz (Baris Ceviz) <me@barisceviz.com>
2020-09-23 05:18:15 +0300

    Add extensions for Microsoft.Extensions.DependencyInjection package

---
+++ src/Nano.DependencyInjector/NanoDependencyInjectorExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Nano.DependencyInjector
+{
+    public static class NanoDependencyInjectorExtensions
+    {
+        public static IServiceCollection RegisterDependencies(this IServiceCollection services, Assembly assembly)
+        {
+            var dependencies = DependencyInjectionConfigure.GetDependencies(assembly);
+
+            foreach (var dependency in dependencies)
+            {
+                services.Add(
+                    new ServiceDescriptor(dependency.InterfaceType, dependency.Implementation, dependency.Type));
+            }
+
+            return services;
+        }
+    }
+}
