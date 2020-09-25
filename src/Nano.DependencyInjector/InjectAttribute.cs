0f5aeeca06cea85cf162a7af0f33533fb104224b
emrekara37 <emre.kara5@ogr.sakarya.edu.tr>
2020-09-26 00:04:18 +0300

    Property injection implemented

---
+++ src/Nano.DependencyInjector/InjectAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nano.DependencyInjector
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class InjectAttribute : Attribute
+    {
+
+    }
+}
--- src/Nano.DependencyInjector/NanoDependencyInjectorExtensions.cs
+++ src/Nano.DependencyInjector/NanoDependencyInjectorExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Linq;
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Nano.DependencyInjector
@@ -6,16 +8,91 @@ namespace Nano.DependencyInjector
     public static class NanoDependencyInjectorExtensions
     {
         public static IServiceCollection RegisterDependencies(this IServiceCollection services, Assembly assembly)
-        {
+            {
             var dependencies = DependencyInjectionConfigure.GetDependencies(assembly);
 
             foreach (var dependency in dependencies)
             {
-                services.Add(
-                    new ServiceDescriptor(dependency.InterfaceType, dependency.ImplementationType, dependency.LifeTime));
+
+                if (dependency.HasPropertyInjectAttribute())
+                {
+                    services.AddPropertyInjectableTypes(dependency);
+                }
+                else
+                {
+                    services.Add(
+                        new ServiceDescriptor(dependency.InterfaceType, dependency.ImplementationType, dependency.LifeTime));
+                }
+
             }
 
             return services;
+        }
+
+        private static void SetPropertyValue(IServiceProvider provider, object instance)
+        {
+            var injectableProperties = instance.GetType().GetProperties()
+                .Where(p => p.GetCustomAttribute<InjectAttribute>() != null)
+                .ToList();
+            foreach (var property in injectableProperties)
+            {
+                var value = provider.GetService(property.PropertyType);
+                property.SetValue(instance, value);
+            }
+
+        }
+        private static object CreateInstance(IServiceProvider provider, Type type)
+        {
+            object instance;
+            ConstructorInfo publicConstructor = type.GetConstructor(BindingFlags.Instance | BindingFlags.Public,
+                null, new[] { typeof(string) }, null);
+
+
+            if (publicConstructor == null)
+            {
+                instance = System.Runtime.Serialization.FormatterServices
+                    .GetUninitializedObject(type);
+                SetPropertyValue(provider, instance);
+                return instance;
+            }
+
+            var constructorPareParameters = publicConstructor.GetParameters();
+
+            if (constructorPareParameters.Any())
+            {
+                var parameterInstances = constructorPareParameters.Select(parameter => provider.GetRequiredService(parameter.ParameterType)).ToList();
+                instance = Activator.CreateInstance(type, parameterInstances);
+            }
+            else
+            {
+                instance = Activator.CreateInstance(type);
+            }
+            SetPropertyValue(provider, instance);
+            return instance;
+        }
+
+        internal static void AddPropertyInjectableTypes(this IServiceCollection services, DependencyInjectionServiceType dependency)
+        {
+            switch (dependency.LifeTime)
+            {
+                case ServiceLifetime.Scoped:
+                    services.AddScoped(dependency.InterfaceType, provider => CreateInstance(provider, dependency.ImplementationType));
+                    break;
+                case ServiceLifetime.Singleton:
+                    services.AddSingleton(dependency.InterfaceType, provider => CreateInstance(provider, dependency.ImplementationType));
+                    break;
+                case ServiceLifetime.Transient:
+                    services.AddTransient(dependency.InterfaceType, provider => CreateInstance(provider, dependency.ImplementationType));
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        internal static bool HasPropertyInjectAttribute(this DependencyInjectionServiceType dependency)
+        {
+            return dependency.ImplementationType.GetProperties()
+                .Any(p => p.GetCustomAttribute<InjectAttribute>() != null);
         }
     }
 }
